d80272b5b4307f5036f545a8c5d3398c196c799f
Tamara Alarcón <Tamy@MacBook-Pro-de-Tamara.local>
2017-11-05 23:29:25 -0300

    General - cambios server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.listen(process.env.PORT || 8080);
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
+app.get('/*', function(res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
